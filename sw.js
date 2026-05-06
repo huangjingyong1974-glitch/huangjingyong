b9c220dc9a59457a0e67dc01584686903ebd46d0
jingyong <jingyong@jingyong.tail69bd81.ts.net>
2026-05-06 20:22:20 +0800

    Fix Live setup and cache refresh

--- sw.js
+++ sw.js
@@ -1,5 +1,12 @@
-const CACHE_NAME = "sunny-english-v4";
-const APP_FILES = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
+const CACHE_NAME = "sunny-english-v5";
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./styles.css?v=live-setup-v5",
+  "./app.js?v=live-setup-v5",
+  "./manifest.webmanifest",
+  "./icon.svg",
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES)));
@@ -15,6 +22,12 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.open(CACHE_NAME).then((cache) => cache.match(event.request).then((cached) => cached || fetch(event.request))),
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request)),
   );
 });
